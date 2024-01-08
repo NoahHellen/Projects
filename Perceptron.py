56eb271b31068ff6bd1dce50f4ad7a8e778f6c52
NoahHellen <skpv43@durham.ac.uk>
2024-01-08 23:58:51 +0000

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -1,39 +1,39 @@
-import codecademylib3_seaborn
-import matplotlib.pyplot as plt
-import random
+lines = []
 
-def generate_training_set(num_points):
-	x_coordinates = [random.randint(0, 50) for i in range(num_points)]
-	y_coordinates = [random.randint(0, 50) for i in range(num_points)]
-	training_set = dict()
-	for x, y in zip(x_coordinates, y_coordinates):
-		if x <= 45-y:
-			training_set[(x,y)] = 1
-		elif x > 45-y:
-			training_set[(x,y)] = -1
-	return training_set
+class Perceptron:
+  def __init__(self, num_inputs=3, weights=[1,1,1]):
+    self.num_inputs = num_inputs
+    self.weights = weights
 
-training_set = generate_training_set(30)
+  def weighted_sum(self, inputs):
+    weighted_sum = 0
+    for i in range(self.num_inputs):
+      weighted_sum += self.weights[i]*inputs[i]
+    return weighted_sum
 
-x_plus = []
-y_plus = []
-x_minus = []
-y_minus = []
+  def activation(self, weighted_sum):
+    if weighted_sum >= 0:
+      return 1
+    if weighted_sum < 0:
+      return -1
 
-for data in training_set:
-	if training_set[data] == 1:
-		x_plus.append(data[0])
-		y_plus.append(data[1])
-	elif training_set[data] == -1:
-		x_minus.append(data[0])
-		y_minus.append(data[1])
-    
-fig = plt.figure()
-ax = plt.axes(xlim=(-25, 75), ylim=(-25, 75))
+  def training(self, training_set):
+    foundLine = False
+    while not foundLine:
+      total_error = 0
+      for inputs in training_set:
+        prediction = self.activation(self.weighted_sum(inputs))
+        actual = training_set[inputs]
+        error = actual - prediction
+        total_error += abs(error)
+        for i in range(self.num_inputs):
+          self.weights[i] += error*inputs[i]
 
-plt.scatter(x_plus, y_plus, marker = '+', c = 'green', s = 128, linewidth = 2)
-plt.scatter(x_minus, y_minus, marker = '_', c = 'red', s = 128, linewidth = 2)
+      slope = -self.weights[0]/self.weights[1]
+      intercept = -self.weights[2]/self.weights[1]
+      y1 = (slope * 0) + intercept
+      y2 = (slope * 50) + intercept
+      lines.append([[0,50], [y1, y2]])
 
-plt.title("Training Set")
-
-plt.show()
+      if total_error == 0:
+        foundLine = True
