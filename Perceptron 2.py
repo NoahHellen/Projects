5d6a8cfc403e541f89fec6434fbe0d7fe985e65b
NoahHellen <skpv43@durham.ac.uk>
2024-01-14 12:19:30 +0000

    Update Perceptron 2.py

---
+++ Perceptron 2.py
@@ -0,0 +1,36 @@
+import numpy as np
+features = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+labels = np.array([0, 0, 0, 1])
+w = [0.9, 0.9]
+threshold = 0.5
+learning_rate = 0.1
+epoch = 20
+for j in range(0, epoch):
+    print("epoch ", j)
+    global_delta = 0
+    for i in range(0, features.shape[0]):
+        actual = labels[i]
+        instance = features[i]
+
+        x0 = instance[0]
+        x1 = instance[1]
+
+        sum_unit = x0 * w[0] + x1 * w[1]
+
+        if sum_unit > threshold:
+            fire = 1
+        else:
+            fire = 0
+        
+        delta = actual - fire
+        global_delta = global_delta + abs(delta)
+
+        print("preiction:", fire, " whereas actual was ", actual," (error:", delta,")")
+
+        w[0] = w[0] + delta * learning_rate
+        w[1] = w[1] + delta * learning_rate
+    print("------------------")
+    print(w[0], w[1])
+
+    if global_delta == 0:
+        break
